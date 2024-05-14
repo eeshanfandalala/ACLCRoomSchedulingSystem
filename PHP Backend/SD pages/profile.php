3086ad797003477f0eca01622bdec86f517b6422
chanchanK <138700316+chanchanK@users.noreply.github.com>
2024-05-15 00:40:58 +0800

    modifyGamay

--- PHP Backend/SD pages/profile.php
+++ PHP Backend/SD pages/profile.php
@@ -5,7 +5,7 @@ $user_id = $_SESSION['sd_id'];
 $sql = mysqli_query($con, "SELECT * FROM `sd_tb` WHERE `SD_id` = '$user_id'");
 while ($row = mysqli_fetch_array($sql)) {
     $sd_id = $row['SD_id'];
-    ?>
+?>
 
     <body>
 
@@ -14,23 +14,19 @@ while ($row = mysqli_fetch_array($sql)) {
             <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
                 <div>
                     <img src="./profile_pictures/<?php echo $row['SD_pic'];
-                    $profpic = $row['SD_pic']; ?>" alt="profile picture">
+                                                    $profpic = $row['SD_pic']; ?>" alt="profile picture">
 
                     <input type="file" name="profile_pic" id="profile_pic">
                 </div>
                 <div>
-                    <input type="text" name="SD_lastname" id="SD_lastname" value="<?php echo $row['SD_lastname']; ?>"
-                        disabled placeholder="Last Name">
-                    <input type="text" name="SD_firstname" id="SD_firstname" value="<?php echo $row['SD_firstname']; ?>"
-                        disabled placeholder="First Name">
+                    <input type="text" name="SD_lastname" id="SD_lastname" value="<?php echo $row['SD_lastname']; ?>" disabled placeholder="Last Name">
+                    <input type="text" name="SD_firstname" id="SD_firstname" value="<?php echo $row['SD_firstname']; ?>" disabled placeholder="First Name">
                 </div>
                 <div>
-                    <input type="email" name="SD_email" id="SD_email" value="<?php echo $row['SD_email']; ?>" disabled
-                        placeholder="email">
-                    <input type="number" name="SD_number" id="SD_number" value="<?php echo $row['SD_number']; ?>" disabled
-                        placeholder="number">
+                    <input type="email" name="SD_email" id="SD_email" value="<?php echo $row['SD_email']; ?>" disabled placeholder="email">
+                    <input type="number" name="SD_number" id="SD_number" value="<?php echo $row['SD_number']; ?>" disabled placeholder="number">
                     <!-- <input type="text" name="teacher_password" id="teacher_password" disabled value="<?php //echo $row['teacher_password']; 
-                        ?>"> -->
+                                                                                                            ?>"> -->
                 </div>
                 <button type="submit" name="update_btn" id="update_btn" style="display: none;">Update</button>
             </form>
@@ -48,14 +44,14 @@ while ($row = mysqli_fetch_array($sql)) {
 
 
     <script>
-        document.getElementById('edit_btn').addEventListener('click', function () {
+        document.getElementById('edit_btn').addEventListener('click', function() {
             var inputs = document.querySelectorAll('input[type="text"], input[type="email"], input[type="number"], input[type="password"]');
             var updateBtn = document.getElementById('update_btn');
             for (var i = 0; i < inputs.length; i++) {
                 inputs[i].disabled = !inputs[i].disabled;
             }
             // Check if any input is disabled
-            var anyInputDisabled = Array.from(inputs).some(function (input) {
+            var anyInputDisabled = Array.from(inputs).some(function(input) {
                 return input.disabled;
             });
 
@@ -63,7 +59,7 @@ while ($row = mysqli_fetch_array($sql)) {
             updateBtn.style.display = anyInputDisabled ? 'none' : 'block';
         });
     </script>
-    <?php
+<?php
 }
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
@@ -114,7 +110,11 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
                 $insertDefaultSDVal->bind_param("sss", $deafaultSDValues['Email'], $hashedpassword, $deafaultSDValues['SD_pic']);
                 $insertDefaultSDVal->execute();
                 if ($insertDefaultSDVal->affected_rows > 0) {
-                    echo "<script>alert('Account has been changed to default successfully')</script>";
+                    echo "<script>alert('Account has been changed to default successfully you will be logged out!')</script>";
+                    if (isset($_SESSION['sd_id'])) {
+                        unset($_SESSION['sd_id']);
+                        echo "<script>window.location.href = 'index.html'; </script>";
+                    }
                 } else {
                     echo "<script>alert('Failed to insert default SD values')</script>";
                 }
@@ -122,7 +122,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
                 echo "<script>alert('Failed to prepare statement for inserting default SD values')</script>";
             }
         } else {
-            echo "<script>alert('Failed to truncate SD table')</script>";
+            echo "<script>alert('Failed to truncate SD Details')</script>";
         }
     }
 }
