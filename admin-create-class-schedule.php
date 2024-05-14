82c3c2de1c205aa97a1a07c5038ff78beace35b6
eeshanfandalala <ltfelfarra11@gmail.com>
2024-05-15 00:18:31 +0800

    forgotten sidebar xD

--- admin-create-class-schedule.php
+++ admin-create-class-schedule.php
@@ -21,7 +21,7 @@ if (!isset($_SESSION['sd_id'])) {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Manage Account</title>
             <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-            <link rel="stylesheet" href="css/admin-sidebar.css">
+            <link rel="stylesheet" href="css/SD/sidebar.css">
             <link rel="stylesheet" href="css/table.css">
         </head>
 
